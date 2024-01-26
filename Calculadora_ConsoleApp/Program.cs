8338d421254d5352bddabe1a9b8a81d2f6b1f899
Joao Melo <joaovictordgmelo@hotmail.com>
2024-01-26 15:16:56 -0300

    Criando título e menu de opções

--- Calculadora_ConsoleApp/Program.cs
+++ Calculadora_ConsoleApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Criar uma calculadora simples com as funções somar, subtrair, multiplicar, dividir e exibir resultado
+// ver possibilidade de usar classes e orientação de objetos
+
+Console.WriteLine(@"
+░█████╗░░█████╗░██╗░░░░░░█████╗░██╗░░░██╗██╗░░░░░░█████╗░██████╗░░█████╗░██████╗░░█████╗░
+██╔══██╗██╔══██╗██║░░░░░██╔══██╗██║░░░██║██║░░░░░██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗
+██║░░╚═╝███████║██║░░░░░██║░░╚═╝██║░░░██║██║░░░░░███████║██║░░██║██║░░██║██████╔╝███████║
+██║░░██╗██╔══██║██║░░░░░██║░░██╗██║░░░██║██║░░░░░██╔══██║██║░░██║██║░░██║██╔══██╗██╔══██║
+╚█████╔╝██║░░██║███████╗╚█████╔╝╚██████╔╝███████╗██║░░██║██████╔╝╚█████╔╝██║░░██║██║░░██║
+░╚════╝░╚═╝░░╚═╝╚══════╝░╚════╝░░╚═════╝░╚══════╝╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝");
+
+Console.WriteLine("Bem vindo a calculadora de console!");
+Console.WriteLine("\nEscolha uma das operações a seguir: ");
+Console.WriteLine("1. Somar (+)\n2. Subtrair (-)\n3. Multiplicar (X)\n4. Dividir (÷)\n5. Sair");
+Console.Write("\nDigite sua escolha: ");
+int opcaoEscolhida = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Opção escolhida {opcaoEscolhida}");
